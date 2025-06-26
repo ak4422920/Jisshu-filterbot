f472ce7c39c315cd544504f60c5062ed3f1a3fd0
ak4422920 <ak4422920@gmail.com>
2025-06-26 09:48:26 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -50,10 +50,10 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
             [
                 [
                     InlineKeyboardButton(
-                        "♻️ Redeem Here ♻️", url="http://t.me/NehaTestBot"
+                        "♻️ Redeem Here ♻️", url="http://t.me/AkMoviesHubbot"
                     )
                 ],
-                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/IM_JISSHU")],
+                [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/The_SonGoku")],
             ]
         )
 
