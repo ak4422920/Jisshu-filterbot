976e347d4d5935750c152ca13b0dfb052b1c384a
ak4422920 <ak4422920@gmail.com>
2025-07-22 19:51:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/+U9ABfC7hu1EyZjU1")
 MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/movie_seriesgroup")
 
 # Verification
-IS_VERIFY = is_enabled("IS_VERIFY", True)
+IS_VERIFY = is_enabled("IS_VERIFY", False)
 # ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/how_to_open_link_ak")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_open_link_ak")
